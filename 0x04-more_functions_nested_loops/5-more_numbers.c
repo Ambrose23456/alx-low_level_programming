ea50cb6d6fae86165ddd1b2811df6a645165eda1
Ambrose23456 <onyemakonorambrose@gmail.com>
2021-09-02 12:40:10 -0700

    5 fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,16 +10,14 @@ void more_numbers(void)
 	int num;
 	int i = 0;
 
-	while (i < 11)
+	while (i <= 10)
 	{
 		num = 0;
 		while (num <= 14)
 		{
 			if (num >= 10)
-			{
 				_putchar((num / 10) + '0');
-				_putchar((num % 10) + '0');		
-			}
+			_putchar((num % 10) + '0');
 			num++;
 		}
 		i++;
