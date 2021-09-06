24b17c90dda950d06e6fb987b00a99bbca07774e
Ambrose23456 <onyemakonorambrose@gmail.com>
2021-09-06 01:37:42 -0700

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - function to dereference an integer
+ *@n: pointer to an integer address
+ *
+ *Return: Always 0;
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x01 - changes the value of a pointer
