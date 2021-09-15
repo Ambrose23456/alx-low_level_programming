fe3d33031e867e5d95c12c06bf038536683a5659
Ambrose23456 <onyemakonorambrose@gmail.com>
2021-09-15 14:38:01 -0700

    2-strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,10 +8,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	static int length = 1;
+	int length = 0;
 
-	if (*s != '\0')
-	{
-		length++;
-		return (1 + _strlen_recursion(s + 1));
-	}
+	if (*s == 0)
+		return (0);
+
+	length = _strlen_recursion(s + 1);
+
+	return (length + 1);
+}
