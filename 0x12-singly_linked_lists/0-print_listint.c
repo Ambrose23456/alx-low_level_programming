92d87f095d4048b125f9fcb8c6a7643d42c1b834
Ambrose23456 <onyemakonorambrose@gmail.com>
2021-10-13 14:02:47 -0700

    0-printlist.c

---
+++ 0x12-singly_linked_lists/0-print_listint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ *print_listint - function to print linked list in C
+ *@h: pointer to the node
+ *
+ *Return: returns count
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int count = 0;
+
+	if (!h)
+	{
+		return (0);
+	}
+	else
+	{
+		while (h != NULL)
+		{
+		printf("%d", h->n);
+		h = h->next;
+		count++;
+		}
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/1-listint_len.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+}listint_t;
+
+/**
+ *listint_len - calculates the lenght of a linked list
+ *@h: pointer to the node
+ *
+ *Return: length of the linked list or NULL
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	struct node *current = h;
+	unsigned int count = 0;
+
+	if (!h)
+		return NULL;
+	while (current != NULL)
+	{
+		for (current = head; current != NULL; current++);
+		count++;
+	}
+	return (count);
+}
