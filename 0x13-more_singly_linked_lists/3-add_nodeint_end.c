3c918b486bac7dd68ae2538f5f24a677b344e3ff
Ambrose23456 <onyemakonorambrose@gmail.com>
2021-10-13 18:10:24 -0700

    03 add node at the end of the node

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,27 +4,41 @@
 
 /**
  *add_nodeint_end - add nodes to the end of the linked list
- *@int: data to be passed into node
+ *@head: pointer to struct
+ *@n: data to be passed into node
  *
  *Return: nothing
  */
-listint_t *add_nodeint_end(listint_t **head, const int)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
 
-	if (head == NULL)
+	if (new_node)
+	{
+		new_node->n = n;
+		new_node->next = NULL;
+	}
+	else
+	{
+		exit(92);
+	}
+
+	if (*head == NULL)
 	{
 		*head = new_node;
 	}
 	else
 	{
 		listint_t *lastNode = *head;
+		int i = 0;
 
-		while (lastNode != NULL)
+		while (lastNode->next != NULL)
 		{
+			i++;
 			lastNode = lastNode->next;
 		}
 		lastNode->next = new_node;
 	}
+	return (*head);
 
 }
