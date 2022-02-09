e06881ae371f183abcef51ac38ecfb3852332ddc
Ambrose23456 <Onyemakonorambrose@gmail.com>
2022-02-09 08:16:31 -0800

    fix for task 5 - 6

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-'''
-Technical review preperation
-'''
+"""5-island_perimeter module"""
+
 
 def island_perimeter(grid):
-    '''
-    A function that returns the perimeter of the island 
-    described in grid
-    '''
-    perimeter = 0 
-    
-    for r in range(0, len(grid)):
-        for c in range(0, len(grid[r])):
-            if grid[r][c] == 1:
-                if grid[r - 1][c] == 0:
-                    perimeter += 1
-                if grid[r + 1][c] == 0:
-                    perimeter += 1
-                if grid[r][c - 1] == 0:
-                    perimeter += 1
-                if grid[r][c + 1] == 0:
-                    perimeter += 1
-    return perimeter
+    """Returns the perimeter of the island described in grid"""
+    c = 0
+    length = len(grid) - 1
+    width = len(grid[0]) - 1
+
+    for i, r in enumerate(grid):
+        for j, n in enumerate(r):
+            if n == 1:
+                if i == 0 or grid[i - 1][j] != 1:
+                    c += 1
+                if j == 0 or grid[i][j - 1] != 1:
+                    c += 1
+                if j == width or grid[i][j + 1] != 1:
+                    c += 1
+                if i == length or grid[i + 1][j] != 1:
+                    c += 1
+    return c
